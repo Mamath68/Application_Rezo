51ad58942d6cd555a07056df6bace216e92023f4
Mathieu Stamm <108459682+Mamath68@users.noreply.github.com>
2025-03-19 15:24:03 +0100

    branch main

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Home on main',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
